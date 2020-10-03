7b4a16427a377e3b235701bc8cbeca2b9e002359
Arthur Gareginyan <arthurgareginyan@gmail.com>
2020-10-03 20:25:16 +0300

    New feature: Restoring screen position after saving changes

--- inc/php/controls.php
+++ inc/php/controls.php
@@ -161,6 +161,27 @@ function spacexchimp_p014_control_list( $name, $items, $label, $help, $default )
 }
 
 /**
+ * Generator of the hidden option for saving plugin settings to database
+ */
+function spacexchimp_p014_control_hidden( $name, $value ) {
+
+    // Put value of plugin constants into an array for easier access
+    $plugin = spacexchimp_p014_plugin();
+
+    // Generate a part of table
+    $out = "<input
+                type='hidden'
+                name='" . $plugin['settings'] . "_settings[$name]'
+                id='" . $plugin['settings'] . "_settings[$name]'
+                value='$value'
+                class='control-hidden $name'
+            >";
+
+    // Print the generated part of table
+    echo $out;
+}
+
+/**
  * Generator of the separator between option groups
  */
 function spacexchimp_p014_control_separator( $text=null ) {
